9d3a66c3f145f7cd955fcccc7712a963cd3ff1cf
red.lizard.851 <red.lizard.851@gmail.com>
2019-02-22 20:51:18 +0300

    Blueprint functionality has been tested successfully with posts.

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from config import Configuration
+from posts.blueprint import posts
 #Импортируем класс Flask из модуля flask установленного
 app = Flask(__name__)
 app.config.from_object(Configuration)
+app.register_blueprint(posts, url_prefix="/blog")
 #Создание переменной арр конструктором класса Flask
 #__name__ - имя текущего файла, передаваемое в конструктор
 #flask, отталкиваясь от имени файла и пути к нему ищет отальные, необходимые для работы элементы
--- view.py
+++ view.py
@@ -1,6 +1,10 @@
 from app import app
+from flask import render_template
+#flask.render_template(template_name_or_list, **context)
 
 
 @app.route('/')
 def index():
-	return 'Hello World!'
+	name = 'Ivan'
+	return render_template('index.html', n=name)
+	#Функция ищет шаблоны в папке Templates, которая должна быть расположена впапке текушего файла
